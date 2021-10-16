00ff0746d9745b802e8b44ec78c4be7a2216b979
Kal <info@kalacademy.org>
2021-10-16 11:12:08 -0700

    started api project

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const db = require('./config/keys');
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 const app = express();
 
 //Let's write our first route
-app.get('/', (req, res) => res.send('Hello'));
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts)
+
+//Connect to db
+mongoose.connect(db.mongoURI)
+        .then(() => console.log('Mongodb connected'))
+        .catch((err) => console.log(err) );
+
 
 const port = 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`) );
