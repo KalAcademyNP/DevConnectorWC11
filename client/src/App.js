c18da1014a798f2589f8a0170bf21481219f5cdc
Kal <info@kalacademy.org>
2021-11-13 11:05:15 -0800

    Added layout and auth components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
+import Footer from './components/layout/Footer';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>My React App</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<Landing />} />
+            <Route exact path="/register" element={<Register />} />
+            <Route exact path="/login" element={<Login />} />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
+    )
+  }
 }
 
 export default App;
+
